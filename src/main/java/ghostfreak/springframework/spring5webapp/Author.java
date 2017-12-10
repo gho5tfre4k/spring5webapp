4267c04e1ba7e0c0c2ee13a5d0b95bea41bff095
gho5tfre4k <gauravbrode07@gmail.com>
2017-12-10 17:54:07 +0530

    Initial commit

---
+++ src/main/java/ghostfreak/springframework/spring5webapp/Author.java
@@ -0,0 +1,88 @@
+package ghostfreak.springframework.spring5webapp;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+    public Author(String firstName, String lastName){
+         this.firstName = firstName;
+         this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(Id, author.Id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
